09a4b0d3af5ef53ae227cb1331f40cdc2db3436d
Daniel Gibson <danielgibson@Daniels-MacBook-Air.local>
2019-06-04 15:02:22 +1200

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -114,7 +114,7 @@ class Contact extends Component {
                     {/* CONTACT FORM */}
                     <div className="col-md-8">
                       <div className="form-holder">
-                        {
+                        {/* {
                           formSubmitted
                             ? (
                               <p>Thanks for contacting us! We will get in touch with you shortly.</p>
@@ -129,7 +129,81 @@ class Contact extends Component {
                               />
 
                             )
-                        }
+                        } */}
+                        <form
+                          name="contactform-v2"
+                          method="POST"
+                          data-netlify="true"
+                          data-netlify-honeypot="bot-field"
+                          className="row contact-form"
+                          // onSubmit={onFormSubmit}
+                        >
+                          <input
+                            type="hidden"
+                            name="bot-field"
+                          />
+                          <input
+                            type="hidden"
+                            name="form-name"
+                            value="contact"
+                          />
+                          {/* Contact Form Input */}
+                          <div id="input-name" className="col-lg-6">
+                            <input
+                              required
+                              type="text"
+                              name="name"
+                              className="form-control name"
+                              placeholder="Your Name"
+                              // value={name}
+                              // onChange={onValueChange}
+                            />
+                          </div>
+                          <div id="input-email" className="col-lg-6">
+                            <input
+                              required
+                              type="text"
+                              name="email"
+                              className="form-control email"
+                              placeholder="Email Address"
+                              // value={email}
+                              // onChange={onValueChange}
+                            />
+                          </div>
+                          <div id="input-subject" className="col-lg-12">
+                            <input
+                              required
+                              type="text"
+                              name="subject"
+                              className="form-control subject"
+                              placeholder="What's this about?"
+                              // value={subject}
+                              // onChange={onValueChange}
+                            />
+                          </div>
+                          <div id="input-message" className="col-lg-12 input-message">
+                            <textarea
+                              className="form-control message"
+                              name="message"
+                              rows={6}
+                              placeholder="Your Message ..."
+                              // value={message}
+                              // onChange={onValueChange}
+                            />
+                          </div>
+
+                          {/* Contact Form Button */}
+                          <div className="col-lg-12 m-top-10 form-btn">
+                            <button type="submit" className="btn btn-md submit btn-arrow">
+                              <span>Send Message <i className="fas fa-angle-double-right" /></span>
+                            </button>
+                          </div>
+
+                          {/* Contact Form Message */}
+                          <div className="col-lg-12 contact-form-msg">
+                            <span className="loading" />
+                          </div>
+                        </form>
                       </div>
                     </div>	{/* END CONTACT FORM */}
                     {/* END CONTACT FORM */}
